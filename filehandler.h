53de6ac2e4b5a854ea365e5f1b367f79c93b83b8
Trseeds <108378832+Trseeds@users.noreply.github.com>
2025-09-25 10:45:33 -0400

    Update filehandler.h

--- filehandler.h
+++ filehandler.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
+int CloseFile(FILE* pFile);
 int CheckFile(char* pFilename);
 int MakeFile(char* pFilename);
 FILE* OpenFileNW(char* pFilename);
 int DeleteFileNW(char* pFilename);
-int CloseFile(FILE* pFile);
 int AppendFile(char* pFilename, char* pText);
 
 #endif
